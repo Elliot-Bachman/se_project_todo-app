1b1a718382ee332b11c510b857944146940a67d6
ElliotBachman <elliotbachmanx@gmail.com>
2024-10-07 14:32:13 -0500

    Small deletions in index.js

--- components/Todo.js
+++ components/Todo.js
@@ -1,21 +1,20 @@
 class Todo {
-  constructor(data, selector, handleCheck, handleDelete, handleTotal) {
+  constructor(data, selector, handleCheck, handleDelete) {
     this._data = data;
     this._templateElement = document.querySelector(selector);
     this._handleCheck = handleCheck;
     this._handleDelete = handleDelete;
-    this._handleTotal = handleTotal;
   }
 
   _setEventListeners() {
     this._todoDeleteBtn.addEventListener("click", () => {
-      this._handleDelete(this._data.completed);
+      this._handleDelete();
       this._todoElement.remove();
     });
 
     this._todoCheckboxEl.addEventListener("change", () => {
-      this._data.completed = !this._data.completed;
-      this._handleCheck(this._data.completed);
+      this._data.completed = !this._data.completed; //toggle completed state
+      this._handleCheck(this._data.completed); // call checkbox handler
     });
   }
 
